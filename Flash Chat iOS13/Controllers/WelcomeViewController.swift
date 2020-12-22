ce36d3cf6c12ae75acc4e2c1ec51b30b3dd94c60
mdmarufprodhan <marufprodhan463@gmail.com>
2020-12-23 03:21:24 +0600

    Using Constant

--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -16,11 +16,10 @@ class WelcomeViewController: UIViewController {
         super.viewDidLoad()
         titleLabel.text = ""
         var charIndex = 0.0
-        let titleText = "⚡️FlashChat"
+        let titleText = K.appName
         for letter in titleText {
             Timer.scheduledTimer(withTimeInterval: 0.1*charIndex, repeats: false) {(timer) in
                 self.titleLabel.text?.append(letter)
-
             } 
           charIndex += 1
         }
